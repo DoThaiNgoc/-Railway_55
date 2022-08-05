bd0edb5e54d2386e3220712d0e48d5fdf25a5dbe
DoThaiNgoc <tngoc19971998@gmail.com>
2022-08-05 21:53:10 +0700

    Testing_System_2.sql

--- SQL/Testing_System_2.sql
+++ SQL/Testing_System_2.sql
@@ -1,246 +1,275 @@
 
--- TẠO DATABASE TÊN : ASM1
-DROP DATABASE IF EXISTS ASM1;
-CREATE DATABASE ASM1;
+-- TẠO DATABASE TÊN : TestingSystem
+DROP DATABASE IF EXISTS TestingSystem;
+CREATE DATABASE TestingSystem;
 
 -- SỬ DỤNG DATABASE
-USE ASM1;
+USE TestingSystem;
 
--- TẠO BẢNG TÊN : department
-
-DROP TABLE IF EXISTS department;
-CREATE TABLE department
+-- TẠO BẢNG TÊN : Department
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department
 (
-	DepartmentID				INT PRIMARY KEY AUTO_INCREMENT,
-	DepartmentName				VARCHAR(255) UNIQUE KEY 
+	DepartmentID		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	DepartmentName		NVARCHAR(30) NOT NULL UNIQUE KEY 
 );
 
 -- THÊM DỮ LIỆU VÀO BẢNG department :
-
-INSERT INTO department (DepartmentName)
+INSERT INTO Department (DepartmentName)
 VALUES					
-						('Marketing'),
-						('Sale'),
-						('Bao ve'),
-						('Nhan su'),
-						('Ky su');
+						('Marketing' ),
+						('Sale' ),
+						('Bảo vệ' ),
+						('Nhân sự' ),
+						('Kỹ thuật' ),
+						('Tài chính' ),
+						('Phó giám đốc'),
+						('Giám đốc' ),
+						('Thư kí' ),
+						('No person' ),
+						('Bán hàng' );
 
--- TẠO BẢNG TÊN : position
-
-DROP TABLE IF EXISTS `position`;
-CREATE TABLE `position`
+-- TẠO BẢNG TÊN : Position
+DROP TABLE IF EXISTS `Position`;
+CREATE TABLE `Position`
 (
-	PositionID					INT PRIMARY KEY AUTO_INCREMENT,
-	PositionName				ENUM('Dev','Test', 'Scrum Master','PM')
+	PositionID		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	PositionName	ENUM('Dev','Test', 'Scrum Master','PM') NOT NULL UNIQUE KEY
 );
 
--- THÊM DỮ LIỆU VÀO BẢNG `position` :
-
-INSERT INTO `position`	(PositionID, PositionName)
+-- THÊM DỮ LIỆU VÀO BẢNG `Position` :
+INSERT INTO `Position`	(PositionName)
 VALUES
-						(1, 'Dev'),
-						(2, 'Test'),
-						(3, 'Scrum Master'),
-						(4, 'PM');
+						('Dev'),
+						('Test'),
+						('Scrum Master'),
+						('PM');
  
- -- TẠO BẢNG TÊN : account
- 
-DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account`
+ -- TẠO BẢNG TÊN : Account
+DROP TABLE IF EXISTS `Account`;
+CREATE TABLE `Account`
 	(
-		AccountID					INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		Email						VARCHAR(255) NOT NULL UNIQUE KEY,
-		Username					VARCHAR(255) NOT NULL UNIQUE KEY,
-		FullName					VARCHAR(255) NOT NULL,
-		DepartmentID				INT NOT NULL,
-		PositionID					INT NOT NULL,
-		CreateDate					DATETIME,
-		CONSTRAINT fk_account_department FOREIGN KEY (DepartmentID) REFERENCES department (DepartmentID),
-		CONSTRAINT fk_account_Positions FOREIGN KEY (PositionID) REFERENCES `position` (PositionID)
+		AccountID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		Email 			VARCHAR(50) NOT NULL UNIQUE KEY,
+		Username 		VARCHAR(50) NOT NULL UNIQUE KEY,
+		FullName 		NVARCHAR(50) NOT NULL,
+		DepartmentID 	TINYINT UNSIGNED NOT NULL,
+		PositionID 		TINYINT UNSIGNED NOT NULL,
+		CreateDate 		DATETIME DEFAULT NOW(),
+		CONSTRAINT fk_account_department FOREIGN KEY (DepartmentID) REFERENCES Department (DepartmentID),
+		CONSTRAINT fk_account_Positions FOREIGN KEY (PositionID) REFERENCES `Position` (PositionID)
     );
 
--- THÊM DỮ LIỆU VÀO BẢNG `account` :
-
-INSERT INTO `account`	(Email,Username,FullName,DepartmentID,PositionID,CreateDate)
+-- THÊM DỮ LIỆU VÀO BẢNG `Account` :
+INSERT INTO `Account`	(Email,Username,FullName,DepartmentID,PositionID,CreateDate)
 VALUES					
-						('tngoc1997@gmail.com','Ngoc','ĐỖ THÁI NGỌC',5,1,'2022-02-08 21:01:30'),
-						('sondt1996@gmail.com','Sơn','ĐỖ THÁI SƠN',5,1,'2022-02-08 21:01:30'),
-                        ('tranglht1997@gmail.com','Trang','LÊ THỊ HUYỀN TRANG',2,3,'2022-02-08 21:01:30'),
-                        ('ngocgiao2000@gmail.com','Giao','NGÔ NGỌC GIAO',4,4,'2022-02-08 21:01:30'),
-                        ('trtham2001@gmail.com','Thắm','TRẦN THỊ THẮM',2,3,'2022-02-08 21:01:30');
+		('Email1@gmail.com' ,'Username1' ,'Fullname1' , '5' , '1','2020-03-05'),
+		('Email2@gmail.com' ,'Username2' ,'Fullname2' , '1' , '2','2020-03-05'),
+		('Email3@gmail.com' , 'Username3' ,'Fullname3', '2' , '2' ,'2020-03-07'),
+		('Email4@gmail.com' , 'Username4' ,'Fullname4', '3' , '4' ,'2020-03-08'),
+		('Email5@gmail.com' , 'Username5' ,'Fullname5', '4' , '4' ,'2020-03-10'),
+		('Email6@gmail.com' , 'Username6' ,'Fullname6', '6' , '3' ,'2020-04-05'),
+		('Email7@gmail.com' , 'Username7' ,'Fullname7', '2' , '2' , NULL ),
+		('Email8@gmail.com' , 'Username8' ,'Fullname8', '8' , '1' ,'2020-04-07'),
+		('Email9@gmail.com' , 'Username9' ,'Fullname9', '2' , '2' ,'2020-04-07'),
+		('Email10@gmail.com' , 'Username10' ,'Fullname10', '10' , '1' ,'2020-04-09'),
+		('Email11@gmail.com' , 'Username11' ,'Fullname11', '10' , '1' , DEFAULT),
+		('Email12@gmail.com' , 'Username12','Fullname12' , '10' , '1' , DEFAULT);
 
--- TẠO BẢNG TÊN : group
-
-DROP TABLE IF EXISTS `group`;    
-CREATE TABLE `group`
+-- TẠO BẢNG TÊN : Group
+DROP TABLE IF EXISTS `Group`;    
+CREATE TABLE `Group`
 	( 
-		GroupID						INT PRIMARY KEY AUTO_INCREMENT,
-		GroupName					VARCHAR(255),
-		CreatorID					INT NOT NULL,
-		CreateDate					DATETIME,
-		CONSTRAINT fk_group_account FOREIGN KEY (CreatorID) REFERENCES `account` (AccountID)
+		GroupID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		GroupName 		NVARCHAR(50) NOT NULL UNIQUE KEY,
+		CreatorID 		TINYINT UNSIGNED,
+		CreateDate 		DATETIME DEFAULT NOW(),
+		CONSTRAINT fk_group_account FOREIGN KEY (CreatorID) REFERENCES `Account` (AccountID)
 	);
     
--- THÊM DỮ LIỆU VÀO BẢNG `group` :
-
-INSERT INTO `group`		(GroupName,CreatorID,CreateDate)
+-- THÊM DỮ LIỆU VÀO BẢNG `Group` :
+INSERT INTO `Group`		(GroupName,CreatorID,CreateDate)
 VALUES
-						('group1', 1, '2022-01-01 11:01:30'),
-						('group2', 3, '2021-07-08 15:01:25'),
-						('group3', 1, '2019-02-01 17:01:09'),
-						('group4', 4, '2021-06-15 11:01:12'),
-                        ('group5', 5, '2020-10-08 14:01:30');
+		(N'Testing System' , 5,'2019-03-05'),
+		(N'Development' , 1 ,'2020-03-07'),
+		(N'VTI Sale 01' , 2 ,'2020-03-09'),
+		(N'VTI Sale 02' , 3 ,'2020-03-10'),
+		(N'VTI Sale 03' , 4 ,'2020-03-28'),
+		(N'VTI Creator' , 6 ,'2020-04-06'),
+		(N'VTI Marketing 01' , 7 ,'2020-04-07'),
+		(N'Management' , 8 ,'2020-04-08'),
+		(N'Chat with love' , 9 ,'2020-04-09'),
+		(N'Vi Ti Ai' , 10 ,'2020-04-10');
 
--- TẠO BẢNG TÊN : groupAccount
-
-DROP TABLE IF EXISTS groupAccount;
-CREATE TABLE groupAccount 
+-- TẠO BẢNG TÊN : GroupAccount
+DROP TABLE IF EXISTS GroupAccount;
+CREATE TABLE GroupAccount 
 	(
-		GroupID						INT,
-		AccountID					INT,
-		JoinDate					DATETIME,
-		CONSTRAINT fk_groupAccount_group FOREIGN KEY (GroupID) REFERENCES `group` (GroupID),
-		CONSTRAINT fk_groupAccount_account FOREIGN KEY (AccountID) REFERENCES `account` (AccountID)
+		GroupID 		TINYINT UNSIGNED NOT NULL,
+		AccountID 		TINYINT UNSIGNED NOT NULL,
+		JoinDate 		DATETIME DEFAULT NOW(),
+		PRIMARY KEY (GroupID,AccountID),
+		CONSTRAINT fk_groupAccount_group FOREIGN KEY (GroupID) REFERENCES `Group` (GroupID),
+		CONSTRAINT fk_groupAccount_account FOREIGN KEY (AccountID) REFERENCES `Account` (AccountID)
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG groupAccount :
-
-INSERT INTO groupAccount	(GroupID,AccountID,JoinDate)
+-- THÊM DỮ LIỆU VÀO BẢNG GroupAccount :
+INSERT INTO GroupAccount	(GroupID,AccountID,JoinDate)
 VALUES
-							(2, 3, '2021-07-08 15:01:25'),
-							(4, 4, '2021-06-15 11:01:12'),
-							(5, 5, '2020-10-08 14:01:30'),
-							(3, 1, '2019-02-01 17:01:09'),
-							(1, 1, '2022-01-01 11:01:30');
+		( 1 , 1 ,'2019-03-05'),
+		( 1 , 2 ,'2020-03-07'),
+		( 3 , 3 ,'2020-03-09'),
+		( 3 , 4 ,'2020-03-10'),
+		( 5 , 5 ,'2020-03-28'),
+		( 1 , 3 ,'2020-04-06'),
+		( 1 , 7 ,'2020-04-07'),
+		( 8 , 3 ,'2020-04-08'),
+		( 1 , 9 ,'2020-04-09'),
+		( 10 , 10 ,'2020-04-10');
 
--- TẠO BẢNG TÊN : typeQuestion
-
-DROP TABLE IF EXISTS typeQuestion ;
-CREATE TABLE typeQuestion 
+-- TẠO BẢNG TÊN : TypeQuestion
+DROP TABLE IF EXISTS TypeQuestion ;
+CREATE TABLE TypeQuestion 
 	(
-		TypeID						INT PRIMARY KEY AUTO_INCREMENT,
-		TypeName					ENUM('Essay','Multiple-Choice')
+		TypeID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		TypeName 	ENUM('Essay','Multiple-Choice') NOT NULL UNIQUE KEY
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG typeQuestion :
-
-INSERT INTO typeQuestion	(TypeName)
+-- THÊM DỮ LIỆU VÀO BẢNG TypeQuestion :
+INSERT INTO TypeQuestion	(TypeName)
 VALUES
 							('Essay'),
 							('Multiple-Choice'); 
 
--- TẠO BẢNG TÊN : categoryQuestion
-
-DROP TABLE IF EXISTS categoryQuestion;
-CREATE TABLE categoryQuestion 
+-- TẠO BẢNG TÊN : CategoryQuestion
+DROP TABLE IF EXISTS CategoryQuestion;
+CREATE TABLE CategoryQuestion
 	(
-		CategoryID					INT PRIMARY KEY AUTO_INCREMENT,
-		CategoryName				ENUM('Java','.NET','SQL','Postman','Ruby')
+		CategoryID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		CategoryName	NVARCHAR(50) NOT NULL UNIQUE KEY
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG categoryQuestion :
-
-INSERT INTO categoryQuestion	(CategoryName)
+-- THÊM DỮ LIỆU VÀO BẢNG CategoryQuestion :
+INSERT INTO CategoryQuestion	(CategoryName)
 VALUES
-								('Java'),
-								('.NET'),
-								('SQL'),
-								('Postman'),
-								('Ruby');
+		('Java' ),
+		('ASP.NET' ),
+		('ADO.NET' ),
+		('SQL' ),
+		('Postman' ),
+		('Ruby' ),
+		('Python' ),
+		('C++' ),
+		('C Sharp' ),
+		('PHP' );
 
--- TẠO BẢNG TÊN : question
-
-DROP TABLE IF EXISTS question;
-CREATE TABLE question 
+-- TẠO BẢNG TÊN : Question
+DROP TABLE IF EXISTS Question;
+CREATE TABLE Question
 	(
-		QuestionID					INT PRIMARY KEY AUTO_INCREMENT,
-		Content						VARCHAR(255),
-		CategoryID					INT,
-		TypeID						INT,
-		CreatorID					INT,
-		CreateDate					DATETIME,
-		CONSTRAINT fk_question_categoryQuestion FOREIGN KEY (CategoryID) REFERENCES categoryQuestion (CategoryID),
-		CONSTRAINT fk_question_typeQuestion FOREIGN KEY (TypeID) REFERENCES typeQuestion (TypeID),
-		CONSTRAINT fk_group_account1 FOREIGN KEY (CreatorID) REFERENCES `account` (AccountID)
+		QuestionID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		Content 		NVARCHAR(100) NOT NULL,
+		CategoryID 		TINYINT UNSIGNED NOT NULL,
+		TypeID 			TINYINT UNSIGNED NOT NULL,
+		CreatorID 		TINYINT UNSIGNED NOT NULL,
+		CreateDate 		DATETIME DEFAULT NOW(),
+		CONSTRAINT fk_question_categoryQuestion FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID),
+		CONSTRAINT fk_question_typeQuestion FOREIGN KEY (TypeID) REFERENCES TypeQuestion (TypeID),
+		CONSTRAINT fk_group_account1 FOREIGN KEY (CreatorID) REFERENCES `Account` (AccountID)
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG question  :
-
-INSERT INTO question 	(Content,CategoryID,TypeID,CreatorID,CreateDate)
+-- THÊM DỮ LIỆU VÀO BẢNG Question  :
+INSERT INTO Question 	(Content,CategoryID,TypeID,CreatorID,CreateDate)
 VALUES
-						('java khởi chạy ct như nào', 1 , 1 , 1 , '2020-07-08 15:01:25'),
-						('.NET sử dụng lập trình web đúng k' , 2 , 2 , 2 , '2019-07-08 13:01:25'),
-						('học sql nhằm mục đích gì', 3 , 1 , 3 , '2021-07-08 14:01:25'),
-						('Postman là gì', 4 , 1 , 5 , '2019-07-08 15:11:25'),
-						('cấu trúc ruby như nào', 5 , 1 , 4 , '2022-07-08 15:02:19');
+		('Câu hỏi về Java' , 1 , '1' , '2' ,'2020-04-05'),
+		('Câu Hỏi về PHP' , 10 , '2' , '2' ,'2020-04-05'),
+		('Hỏi về C#' , 9 , '2' , '3' ,'2020-04-06'),
+		('Hỏi về Ruby' , 6 , '1' , '4' ,'2020-04-06'),
+		('Hỏi về Postman' , 5 , '1' , '5' ,'2020-04-06'),
+		('Hỏi về ADO.NET' , 3 , '2' , '6' ,'2020-04-06'),
+		('Hỏi về ASP.NET' , 2 , '1' , '7' ,'2020-04-06'),
+		('Hỏi về C++' , 8 , '1' , '8' ,'2020-04-07'),
+		('Hỏi về SQL' , 4 , '2' , '9' ,'2020-04-07'),
+		('Hỏi về Python' , 7 , '1' , '10' ,'2020-04-07');
 
--- TẠO BẢNG TÊN : answer
-
-DROP TABLE IF EXISTS answer;
-CREATE TABLE answer
+-- TẠO BẢNG TÊN : Answer
+DROP TABLE IF EXISTS Answer;
+CREATE TABLE Answer
  (
-	AnswerID					INT PRIMARY KEY AUTO_INCREMENT,
-	Content						VARCHAR(255),
-	QuestionID					INT,	
-	isCorrect					BOOL,
-    CONSTRAINT fk_answer_question FOREIGN KEY (QuestionID) REFERENCES question (QuestionID)
+	AnswerID 		TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	Content 		NVARCHAR(100) NOT NULL,
+	QuestionID		TINYINT UNSIGNED NOT NULL,
+	isCorrect 		BIT DEFAULT 1,
+    CONSTRAINT fk_answer_question FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
 );
 
--- THÊM DỮ LIỆU VÀO BẢNG answer :
-
-INSERT INTO answer		(Content,QuestionID,isCorrect)
+-- THÊM DỮ LIỆU VÀO BẢNG Answer:
+INSERT INTO Answer	(Content,QuestionID,isCorrect)
 VALUES
-						('answer_1', 1, TRUE),
-						('answer_2', 1, FALSE),
-						('answer_3', 3, TRUE),
-						('answer_4', 5, TRUE),
-						('answer_5', 4, TRUE);
+		('Trả lời 01' , 1 , 0),
+		('Trả lời 02' , 1 , 1),
+		('Trả lời 03', 1 , 0 ),
+		('Trả lời 04', 1 , 1 ),
+		('Trả lời 05', 2 , 1 ),
+		('Trả lời 06', 3 , 1 ),
+		('Trả lời 07', 4 , 0 ),
+		('Trả lời 08', 8 , 0 ),
+		('Trả lời 09', 9 , 1 ),
+		('Trả lời 10', 10 , 1 );
 
 -- TẠO BẢNG TÊN : exam
 
-DROP TABLE IF EXISTS exam;
-CREATE TABLE exam 
+DROP TABLE IF EXISTS Exam;
+CREATE TABLE Exam
 	(
-		ExamID						INT PRIMARY KEY AUTO_INCREMENT,
-		Code						INT,
-		Title						VARCHAR(255),
-		CategoryID					INT,
-		Duration					DATETIME,
-		CreatorID					INT,
-		CreateDate					DATETIME,
-		CONSTRAINT fk_exam_categoryQuestion1 FOREIGN KEY (CategoryID) REFERENCES categoryQuestion (CategoryID)
+		ExamID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		`Code` 			CHAR(10) NOT NULL,
+		Title 			NVARCHAR(50) NOT NULL,
+		CategoryID 		TINYINT UNSIGNED NOT NULL,
+		Duration 		TINYINT UNSIGNED NOT NULL,
+		CreatorID 		TINYINT UNSIGNED NOT NULL,
+		CreateDate 		DATETIME DEFAULT NOW(),
+		CONSTRAINT fk_exam_categoryQuestion1 FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID)
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG exam :
-
-INSERT INTO exam		(Code,Title,CategoryID,Duration,CreatorID,CreateDate)
+-- THÊM DỮ LIỆU VÀO BẢNG Exam :
+INSERT INTO Exam		(Code,Title,CategoryID,Duration,CreatorID,CreateDate)
 VALUES
-						('1', 'tiêu đề_1', 2, '2022-07-08  08:00:00', 1, '2022-07-05 15:02:19'),
-						('2', 'tiêu đề_2', 1, '2022-07-08  09:05:00', 2, '2022-07-07 15:02:19'),
-						('3', 'tiêu đề_3', 5, '2022-07-08  10:15:00', 3, '2022-07-05 15:02:19'),
-						('4', 'tiêu đề_4', 1, '2022-07-08  14:20:00', 5, '2022-07-03 15:02:19'),
-						('5', 'tiêu đề_5', 4, '2022-07-08  15:10:00', 1, '2022-07-02 15:02:19');
+		('VTIQ001' , 'Đề thi C#' , 1 , 60 , '5' ,'2019-04-05'),
+		('VTIQ002' , 'Đề thi PHP' , 10 , 60 , '2' ,'2019-04-05'),
+		('VTIQ003' , 'Đề thi C++' , 9 , 120 , '2' ,'2019-04-07'),
+		('VTIQ004' , 'Đề thi Java' , 6 , 60 , '3' ,'2020-04-08'),
+		('VTIQ005' , 'Đề thi Ruby' , 5 , 120 , '4' ,'2020-04-10'),
+		('VTIQ006' , 'Đề thi Postman' , 3 , 60 , '6' ,'2020-04-05'),
+		('VTIQ007' , 'Đề thi SQL' , 2 , 60 , '7' ,'2020-04-05'),
+		('VTIQ008' , 'Đề thi Python' , 8 , 60 , '8' ,'2020-04-07'),
+		('VTIQ009' , 'Đề thi ADO.NET' , 4 , 90 , '9' ,'2020-04-07'),
+		('VTIQ010' , 'Đề thi ASP.NET' , 7 , 90 , '10' ,'2020-04-08');
 
--- TẠO BẢNG TÊN : examQuestion
-
-DROP TABLE IF EXISTS examQuestion;
-CREATE TABLE examQuestion 
+-- TẠO BẢNG TÊN : ExamQuestion
+DROP TABLE IF EXISTS ExamQuestion;
+CREATE TABLE ExamQuestion 
 	(
-		ExamID						INT,	
-		QuestionID					INT,
-		CONSTRAINT fk_examQuestion_exam FOREIGN KEY (ExamID) REFERENCES exam (ExamID),
-		CONSTRAINT fk_examQuestion_question FOREIGN KEY (QuestionID) REFERENCES question (QuestionID)
+		ExamID 			TINYINT UNSIGNED NOT NULL,
+		QuestionID 		TINYINT UNSIGNED NOT NULL,
+        PRIMARY KEY (ExamID,QuestionID),
+		CONSTRAINT fk_examQuestion_exam FOREIGN KEY (ExamID) REFERENCES Exam (ExamID),
+		CONSTRAINT fk_examQuestion_question FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
 	);
 
--- THÊM DỮ LIỆU VÀO BẢNG examQuestion :
-
-INSERT INTO examQuestion	(ExamID,QuestionID)
+-- THÊM DỮ LIỆU VÀO BẢNG ExamQuestion :
+INSERT INTO ExamQuestion	(ExamID,QuestionID)
 VALUES
-							(1, 2),
-							(2, 5),
-							(3, 1),
-							(4, 2),
-							(5, 1);
+		( 1 , 5 ),
+		( 2 , 10 ),
+		( 3 , 4 ),
+		( 4 , 3 ),
+		( 5 , 7 ),
+		( 6 , 10 ),
+		( 7 , 2 ),
+		( 8 , 10 ),
+		( 9 , 9 ),
+		( 10 , 8 );
 
             
 
