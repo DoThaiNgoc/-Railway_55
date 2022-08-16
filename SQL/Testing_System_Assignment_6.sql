3d72f114b5d742518a8bd2dd17fd2a3f342ee56d
DoThaiNgoc <tngoc19971998@gmail.com>
2022-08-17 00:25:16 +0700

    Testing_System_Assignment_6.sql

--- SQL/Testing_System_Assignment_6.sql
+++ SQL/Testing_System_Assignment_6.sql
@@ -45,7 +45,7 @@ DELIMITER $$
 CREATE PROCEDURE ST_PR_GetCountQuesFromType()
 BEGIN
 WITH CTE_MaxTypeID AS(
-SELECT tq.TypeName, count(q.TypeID) AS SL FROM question q
+SELECT q.TypeID, tq.TypeName, count(q.TypeID) AS SL FROM question q
 INNER JOIN typequestion tq ON tq.TypeID = q.TypeID
 GROUP BY q.TypeID)
 SELECT * FROM CTE_MaxTypeID
@@ -54,7 +54,48 @@ END$$
 DELIMITER ;
 Call ST_PR_GetCountQuesFromType();
 
+-- OUT : sử dụng out để lấy giá trị ra
+DROP PROCEDURE IF EXISTS sp_GetCountQuesFromType;
+DELIMITER $$
+CREATE PROCEDURE sp_GetCountQuesFromType(OUT maxIDofQues TINYINT)
+BEGIN
+WITH CTE_CountTypeID AS (
+SELECT count(q.TypeID) AS SL FROM question q
+GROUP BY q.TypeID)
+SELECT q.TypeID INTO maxIDofQues FROM question q
+GROUP BY q.TypeID
+HAVING COUNT(q.TypeID) = (SELECT max(SL) FROM CTE_CountTypeID);
+END$$
+DELIMITER ;
+SET @maxIDofQues =0;
+Call sp_GetCountQuesFromType(@maxIDofQues);
+SELECT @maxIDofQues;
+
 -- Question 5: Sử dụng store ở question 4 để tìm ra tên của type question
+
+SELECT * FROM typequestion WHERE TypeID = @maxIDofQues;
+
+-- Question 6: Viết 1 store cho phép người dùng nhập vào 1 chuỗi và trả về group có tên
+-- chứa chuỗi của người dùng nhập vào hoặc trả về user có username chứa
+-- chuỗi của người dùng nhập vào
+DROP PROCEDURE IF EXISTS SP_GroupName_or_UseName;
+DELIMITER $$
+CREATE PROCEDURE SP_GroupName_or_UseName(IN GR_NAME VARCHAR(50), IN U_Name VARCHAR(50))
+		BEGIN 
+        SELECT G.GroupName,A.Username FROM `Group` G
+        INNER JOIN `Account` A ON A.AccountID = G.CreatorID
+             WHERE G.GroupName=GR_NAME OR A.Username= U_Name;
+        END$$
+DELIMITER ;
+
+CALL  SP_GroupName_or_UseName('Testing System', 'Username2');
+
+SELECT * FROM SP_GroupName_or_UseName;
+
+
+
+
+
 
 
 
