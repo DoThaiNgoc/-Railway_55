73d890b7da4d66fa3da373859c4ca6066ac92a8e
DoThaiNgoc <110244922+DoThaiNgoc@users.noreply.github.com>
2022-09-10 00:48:54 +0700

    Delete Java/tttt directory

--- Java/tttt/Position.java
+++
@@ -1,7 +0,0 @@
-public class Position {
-    public int id;
-    public PositionName name;
-    public enum PositionName
-            Dev, Test, Scrum_Master, PM
-    }
-}
--- Java/tttt/Question.java
+++
@@ -1,7 +0,0 @@
-public class Question {
-    public int id;
-    public String content;
-    public TypeQuestion type;
-    public Account creator;
-    public LocalDate createDate;
-}
